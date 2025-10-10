11282f2391f6fc53a16957d5e8b50fa719d29f0d
MattSteveMitch <mattstevemitch@gmail.com>
2025-10-09 23:27:44 -0600

    initial commit: placeholder content

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+  return <div className="body bg-dark text-light">App will display here</div>;
+}
+
+import {BrowserRouter, NavLink, Route, Routes} from 'react-router-dom';
+import {Login} from "./login/login";
+import {Play} from "./play/play";
+import {Scores} from "./scores/scores";
+import {About} from "./about/about";
+
+
