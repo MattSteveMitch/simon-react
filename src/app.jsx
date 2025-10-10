4ff78ee1398388b1323bb8fbd8a374c47ee9f0f3
MattSteveMitch <mattstevemitch@gmail.com>
2025-10-10 00:08:59 -0600

    NavLinks active, figuring out why placeholder text not rendering

--- src/app.jsx
+++ src/app.jsx
@@ -1,59 +1,8 @@
+
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
-export default function App() {
-  return (
-    <body className="bg-dark text-light">
-      <header className="container-fluid">
-      <nav className="navbar fixed-top navbar-dark">
-        <a className="navbar-brand" href="#">Simon<sup>&reg;</sup></a>
-        <menu className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link active" href="index.html">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="play.html">Play</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="scores.html">Scores</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="about.html">About</a>
-          </li>
-        </menu>
-      </nav>
-    </header>
-
-    <header className="container-fluid">
-      <nav className="navbar fixed-top navbar-dark">
-        <a className="navbar-brand" href="#">Simon<sup>&reg;</sup></a>
-        <menu className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href="index.html">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="play.html">Play</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="scores.html">Scores</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link active" href="about.html">About</a>
-          </li>
-        </menu>
-      </nav>
-    </header>
-
-    <footer className="bg-dark text-white-50">
-      <div className="container-fluid">
-        <span className="text-reset">Author Name(s)</span>
-        <a className="text-reset" href="https://github.com/webprogramming260/simon-css">Source</a>
-      </div>
-    </footer>
-    </body>
-  );
-}
 
 import {BrowserRouter, NavLink, Route, Routes} from 'react-router-dom';
 import {Login} from "./login/login";
@@ -62,3 +11,50 @@ import {Scores} from "./scores/scores";
 import {About} from "./about/about";
 
 
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <body className="bg-dark text-light">
+      <header className="container-fluid">
+      <nav className="navbar fixed-top navbar-dark">
+        <div className="navbar-brand">Simon<sup>&reg;</sup></div>
+        <menu className="navbar-nav">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="">Home</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="play">Play</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="scores">Scores</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="about">About</NavLink>
+          </li>
+        </menu>
+      </nav>
+    </header>
+
+    <footer className="bg-dark text-white-50">
+      <div className="container-fluid">
+        <span className="text-reset">Author Name(s)</span>
+        <NavLink className="text-reset" to="https://github.com/webprogramming260/simon-css">Source</NavLink>
+      </div>
+    </footer>
+    </body>
+    </BrowserRouter>
+  );
+
+  <Routes>
+  <Route path='/' element={<Login />} exact />
+  <Route path='/play' element={<Play />} />
+  <Route path='/scores' element={<Scores />} />
+  <Route path='/about' element={<About />} />
+  <Route path='*' element={<NotFound />} />
+  </Routes>
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+}
